53665a789340216a98c4712e7def7fabdb7d80f5
Mo syed <msyed77072@gmail.com>
2022-01-03 19:13:55 -0600

    updated for web

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 
 
-
-class Blog(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    date = models.DateField()
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='portfolio/images/')
+    url = models.URLField(blank=True)
